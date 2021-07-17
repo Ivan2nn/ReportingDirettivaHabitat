e7b1fd1fd25346eafd12e36728293d65f17edc41
Ivan2nn <i.bernabucci@gmail.com>
2021-07-17 11:49:07 +0200

    Landing page for the 3 report added

---
+++ src/resources/views/basic/landing_3_report.blade.php
@@ -0,0 +1,148 @@
+@extends('partials.layout')
+
+@section('content')
+
+<section class="c-layout-revo-slider c-layout-revo-slider-4" dir="ltr">
+    <div class="tp-banner-container c-theme">
+        <div class="tp-banner rev_slider" data-version="5.0">
+            <ul>
+                <!--BEGIN: SLIDE #1 -->
+                <li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/Rosalia_Alpina.jpg') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" />
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+                    </div>
+                </li>
+                <!--END -->
+                <!--BEGIN: SLIDE #2 -->
+		<li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/Campanula_morettiana.png') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" />
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+		     </div>
+                </li>
+                <!-- <li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/AreaLince.jpg') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600"></div>
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+                </li> -->
+                <!--END -->
+                <!--BEGIN: SLIDE #3 -->
+                <li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/Orso1Ale.jpg') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" />
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+			</div>
+                </li>
+                <!--END -->
+		<!--BEGIN: SLIDE #4 -->
+		<li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/IMG_2286.png') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" />
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+			</div>
+                </li>
+		<!--END -->
+		<!--BEGIN: SLIDE #5 -->
+		<li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/Woodwardia_radicans.png') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" />
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600"></div>
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+                </li>
+		<!--END -->
+		<!--BEGIN: SLIDE #5 -->
+		<li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
+                    <img alt="" src="{!! asset('images/IMG_2641.png') !!}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" />
+                    <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
+                    data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
+                        <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> REPORTING DIRETTIVA HABITAT</h3>
+			</div>
+                </li>
+		<!--END -->
+
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- END: LAYOUT/SLIDERS/REVO-SLIDER-4 -->
+<!-- BEGIN: CONTENT/FEATURES/FEATURES-1 -->
+<div class="c-content-box c-size-md c-bg-white">
+    <div class="container">
+        <div class="section-header">
+            <div class="row">
+                <div class="col-sm-6 focus-box">
+                    <div class="service-icon">
+                        <a href="{{ route('species-basic-search') }}">
+                        <i class="pixeden our-focus-widget-image mimi-focus-home-icon-species"></i> <!-- FOCUS ICON-->
+                        </a>
+                    </div>
+                    <h5 class="red-border-bottom">SPECIE</h5>
+                    <!-- <div class="c-content-feature-1 wow animate fadeInUp">
+                        <div class="c-content-line-icon c-theme c-icon-screen-chart"></div>
+                        <h3 class="c-font-uppercase c-font-bold">Fully responsive</h3>
+                        <p class="c-font-thin">Beautiful cinematic designs optimized for all screen sizes and types. Compatible with Retina high pixel density displays.</p>
+                    </div> -->
+                </div>
+                <div class="col-sm-6 focus-box">
+                    <div class="service-icon">
+                        <a href="{{ route('habitat-basic-search') }}">
+                        <i class="pixeden our-focus-widget-image mimi-focus-home-icon-habitat"></i> <!-- FOCUS ICON-->
+                        </a>
+                    </div>
+                    <h5 class="red-border-bottom">HABITAT</h5>
+                    <!-- <div class="c-content-feature-1 wow animate fadeInUp" data-wow-delay="0.2s">
+                        <div class="c-content-line-icon c-theme c-icon-support"></div>
+                        <h3 class="c-font-uppercase c-font-bold">Visual & Pragmatic</h3>
+                        <p class="c-font-thin">Featuring trending modern web standards.
+                            <br/>Clean and easy framework design for worry and hassle free customizations.</p>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END: CONTENT/FEATURES/FEATURES-1 -->
+
+
+<!-- BEGIN: CONTENT/TABS/TAB-1 -->
+<div class="c-content-box c-size-md c-no-bottom-padding c-overflow-hide">
+    <div class="c-container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="c-content-tab-2 c-theme c-opt-1">
+                    
+                    <div class="c-tab-content">
+                        <div class="c-bg-img-center1" style="background-color= rgba(39, 39, 39, 1);">
+                            <div class="container">
+                                <div class="tab-content">
+                                    <div class="tab-pane fade in active" id="c-tab2-opt1-1">
+                                        <div class="c-tab-pane">
+                                            <h4 class="c-font-30 c-font-thin c-font-uppercase c-font-bold">Reporting Direttiva Habitat</h4>
+                                            <p class="c-font-17 c-margin-b-20 c-margin-t-20 c-font-thin "> Finalizzato alla raccolta, organizzazione e messa a disposizione dei dati e delle cartografie prodotte dall’Italia per il reporting ex Articolo17 della Direttiva "Habitat" 92/43/CEE (3° Rapporto nazionale), trasmesso alla Commissione Europea (CE) nel dicembre 2013, nonché della documentazione utile per l’adempimento delle future attività di monitoraggio e rendicontazione.
+                                            <br><br>
+                                            Nel sito sono disponibili inoltre le schede di monitoraggio contenenti i protocolli di campo specifici per tutte le specie animali e vegetali e tutti gli habitat italiani, terrestri e di acqua dolce, di interesse comunitario (elencati negli allegati All. I, II, IV, V della Direttiva). 
+                                            <br><br>
+                                            Tali schede sono state prodotte nell’ambito di un progetto nazionale concluso nell’ottobre del 2016 e contengono la descrizione delle metodiche e dei protocolli operativi utilizzabili per effettuare le attività di sorveglianza e monitoraggio in campo richieste dalla Direttiva. 
+                                            Nel sito è possibile visualizzare su scala biogeografica dati di distribuzione, stato di conservazione e trend delle specie e degli habitat italiani di direttiva, prodotti per la compilazione del 3° Rapporto nazionale ex Art. 17, ed effettuare interrogazioni e ricerche online sulle liste aggiornate delle specie e degli habitat italiani di interesse comunitario (di seguito chiamate checklists).
+                                            </p>
+                                            
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
