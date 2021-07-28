251eeb8e36ef73fa5ccf7d9aae368df8886b5246
Melina Kukkasela <melina.kukkasela@gmail.com>
2021-07-28 17:52:26 +0300

    Add legenda mobile styles

--- src/resources/views/basic/species-graphic-search.blade.php
+++ src/resources/views/basic/species-graphic-search.blade.php
@@ -284,9 +284,9 @@
 
 				<div class="row">
 					<div class="row c-margin-b-30">
-						<h2 class="c-font-bold" style="margin-left:30px;">Legenda</h2>
+						<h2 class="c-font-bold" style="margin-left:45px;">Legenda</h2>
 					</div>
-					<div class="row">
+					<div class="row c-margin-b-40" style="margin-left: 14px;">
 						<div class="col-sm-3">
 							<h4 class="c-margin-b-20">Regioni Biogeografiche</h4>
 							<ul class="list-unstyled">
@@ -297,7 +297,7 @@
 							</ul>			
 						</div>
 						<div class="col-sm-3">
-							<h4 class="c-margin-b-20">Presenza</h4>
+							<h4 class="c-margin-b-20 h4m">Presenza</h4>
 							<ul class="list-unstyled">
 								<li><span class="c-font-bold">PRE</span> = Presente</li>
 								<li><span class="c-font-bold">OCC</span> = Occasionale</li>
@@ -309,7 +309,7 @@
 							</ul>	
 						</div>
 						<div class="col-sm-3">
-							<h4 class="c-margin-b-20">Stato di Conservazione</h4>
+							<h4 class="c-margin-b-20 h4m">Stato di Conservazione</h4>
 							<ul class="list-unstyled">
 								<li><div class="mimi-legenda-block legenda-green"></div>Favorevole</li>
 								<li><div class="mimi-legenda-block legenda-yellow"></div>Inadeguato</li>
@@ -318,7 +318,7 @@
 							</ul>	
 						</div>
 						<div class="col-sm-3">
-							<h4 class="c-margin-b-20">Trend</h4>
+							<h4 class="c-margin-b-20 h4m">Trend</h4>
 							<ul class="list-unstyled">
 								<li><img src="{!! asset('images/green_up.png') !!}" />In miglioramento</li>
 								<li><img src="{!! asset('images/yellow_stable.png') !!}" />Stabile</li>
