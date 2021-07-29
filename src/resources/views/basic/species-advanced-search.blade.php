98488ad6cfc60ecdb2948cf621b1461fe71e268d
Melina Kukkasela <melina.kukkasela@gmail.com>
2021-07-29 16:20:28 +0300

    Add legenda specie

--- src/resources/views/basic/species-advanced-search.blade.php
+++ src/resources/views/basic/species-advanced-search.blade.php
@@ -173,8 +173,55 @@
 </div> <!-- end of 1. container -->
 
 
-<div class="blaablaa bg-light-grey">
+<div class="bg-light-grey">
   <div class="container">
+    <div class="row c-margin-t-40 c-margin-b-40">
+        <div class="c-margin-b-30">
+          <h2 class="c-font-bold" style="margin-left: 20px">Legenda</h2>
+        </div>
+        <div class="c-margin-b-40" style="margin-left: 14px;">
+          <div class="col-sm-3">
+            <h4 class="c-margin-b-20">Regioni Biogeografiche</h4>
+            <ul class="list-unstyled">
+              <li><span class="c-font-bold">MED</span> = Mediterranea</li>
+              <li><span class="c-font-bold">CON</span> = Continentale</li>
+              <li><span class="c-font-bold">ALP</span> = Alpina</li>
+              <li><span class="c-font-bold">MMED</span> = Marina Mediterranea</li>
+            </ul>     
+          </div>
+          <div class="col-sm-3">
+            <h4 class="c-margin-b-20 h4m">Presenza</h4>
+            <ul class="list-unstyled">
+              <li><span class="c-font-bold">PRE</span> = Presente</li>
+              <li><span class="c-font-bold">OCC</span> = Occasionale</li>
+              <li><span class="c-font-bold">MAR</span> = Marginale</li>
+              <li><span class="c-font-bold">TAX</span> = Tassonomia non definita</li>
+              <li><span class="c-font-bold">EXa</span> = Estinta dopo l'entrata in vigore della DH</li>
+              <li><span class="c-font-bold">EXp</span> = Estinta prima dell'entrata in vigore della DH</li>
+              <li><span class="c-font-bold">NP</span> = Non Presente</li>
+            </ul> 
+          </div>
+          <div class="col-sm-3">
+            <h4 class="c-margin-b-20 h4m">Stato di Conservazione</h4>
+            <ul class="list-unstyled">
+              <li><div class="mimi-legenda-block legenda-green"></div>Favorevole</li>
+              <li><div class="mimi-legenda-block legenda-yellow"></div>Inadeguato</li>
+              <li><div class="mimi-legenda-block legenda-red"></div>Cattivo</li>
+              <li><div class="mimi-legenda-block legenda-grey"></div>Sconosciuto</li>
+            </ul> 
+          </div>
+          <div class="col-sm-3">
+            <h4 class="c-margin-b-20 h4m">Trend</h4>
+            <ul class="list-unstyled">
+              <li><img src="{!! asset('images/green_up.png') !!}" />In miglioramento</li>
+              <li><img src="{!! asset('images/yellow_stable.png') !!}" />Stabile</li>
+              <li><img src="{!! asset('images/red_down.png') !!}" />In peggioramento</li>
+              <li><img src="{!! asset('images/grey_null.png') !!}" />Sconosciuto</li>
+            </ul> 
+          </div>
+
+        </div>
+      </div>
 
     <div class="row c-margin-t-40" v-if="dataAvailable">
       <div class="col-md-6 c-margin-b-30">
