4db7e82b0be46c64dfa3f791425c878d1a836e70
Ivan2nn <i.bernabucci@gmail.com>
2021-08-04 20:28:40 +0200

    Changed text in reference-context

--- src/resources/views/basic/reference-context.blade.php
+++ src/resources/views/basic/reference-context.blade.php
@@ -7,33 +7,36 @@
     <div class="container">
         	<div class="section-context">
             	<p>
-            	La <a href="http://bd.eionet.europa.eu/activities/Reporting/Article_17">Direttiva Habitat (92/43/CEE)</a> e la <a href="http://bd.eionet.europa.eu/activities/Reporting/Article_12">Direttiva Uccelli (2009/147/CE)</a>, rappresentano i pilastri della politica
-				comunitaria per la conservazione della natura. La Direttiva Habitat è stata recepita dall’Italia tramite il
-				Regolamento D.P.R. n.357 dell’8/09/1997 e successive modificazioni e integrazioni. In base a tale
-				Regolamento le <a href="http://www.regione.taa.it/">Regioni e le Province Autonome di Trento e Bolzano</a> devono adottare misure idonee a
-				garantire la salvaguardia e il <a href="{{ route('piano-di-monitoraggio') }}">monitoraggio</a> dello stato di conservazione delle specie e degli habitat di
-				interesse comunitario (elencati negli Allegati I, II, IV, V della Direttiva) e il <a href="https://www.mite.gov.it/">Ministero della Transizione
-				Ecologica</a> (ex Ministero dell’Ambiente e della Tutela del Territorio e del Mare) ogni sei anni ha l’obbligo di
-				redigere e trasmettere alla Commissione Europea un Rapporto nazionale a partire dai risultati del
-				monitoraggio, con informazioni sulle misure di conservazione e sullo stato di conservazione di habitat e
-				specie.</p>
-				<p>L’attuazione della Direttiva richiede quindi la realizzazione di attività di monitoraggio e reporting per
-					l’intero territorio nazionale, sia all’interno che all’esterno dei siti della rete Natura 2000, per verificare la
-					sufficienza della rete nella conservazione di specie e habitat e l’efficacia dell’applicazione delle misure di
-					gestione e conservazione.</p>
-				<p>Dall’entrata in vigore della Direttiva nell’ambito del reporting ex art. 17 gli Stati membri hanno prodotto
-					quattro <b>Rapporti Nazionali</b>, di cui gli ultimi tre comprensivi di valutazioni dello stato di conservazione di
-					specie e habitat per il periodo di riferimento (2° Report: 2001-2006; 3° Report: 2007-2012; 4° Report: 2013-2018). Sulla base dei Rapporti nazionali la <b>Commissione Europea</b> e l’<b>Agenzia Europea per l’Ambiente</b>
-					producono i National Summary, le dashboard, le valutazioni a scala biogeografica e la sintesi dei risultati a
-					livello europeo, pubblicata ogni sei anni nel Report <b>State of Nature in the EU</b>.</p>
+					La Direttiva Habitat (92/43/CEE) e la Direttiva Uccelli (2009/147/CE) rappresentano i pilastri della politica comunitaria 
+					per la conservazione della natura. La Direttiva Habitat è stata recepita dall’Italia tramite il <a href="https://www.minambiente.it/sites/default/files/archivio/allegati/rete_natura_2000/Regolamento_D.P.R._8_settembre_1997_n._357.PDF" target="_blank">Regolamento D.P.R. n.357 
+					dell’8/09/1997</a> e successive modificazioni e integrazioni. In base a tale Regolamento le <b>Regioni e le Province Autonome di Trento e Bolzano</b>
+					devono adottare misure idonee a garantire la salvaguardia e il <b>monitoraggio</b> dello stato di conservazione delle specie e 
+					degli habitat di  interesse comunitario (elencati negli Allegati I, II, IV, V della Direttiva) e 
+					il <b>Ministero della Transizione Ecologica</b> (ex Ministero dell’Ambiente e della Tutela del Territorio e del Mare) 
+					ogni sei anni ha l’obbligo di redigere e trasmettere alla Commissione Europea un <b>Rapporto nazionale</b> a partire 
+					dai risultati del monitoraggio, con informazioni sulle misure di conservazione e sullo stato di conservazione di habitat e specie. </p>
+
+				<p>L’attuazione della Direttiva richiede quindi la realizzazione di attività di monitoraggio e reporting 
+					per l’intero territorio nazionale, sia all’interno che all’esterno dei siti della rete Natura 2000, 
+					per verificare la sufficienza della rete nella conservazione di specie e habitat e 
+					l’efficacia dell’applicazione delle misure di gestione e conservazione.</p>
 				
-				<p>Gli Attori coinvolti nelle attività di monitoraggio e reporting in Italia sono il Ministero della Transizione
-					Ecologica, le Regioni e le Province Autonome di Trento e Bolzano e l’<a href="https://www.isprambiente.gov.it/it">ISPRA</a>che, dal 2011, coordina le
-					attività per la realizzazione dei Rapporti Nazionali ex Art. 17 e fornisce Linee guida e strumenti tecnico-
-					scientifici per la realizzazione dei monitoraggi. Per conseguire tali finalità l’ISPRA si avvale della
-					collaborazione delle principali <b>Società Scientifiche nazionali</b>, quali: la Società Botanica Italiana, l’Unione
-					Zoologica Italiana, la Societas Herpetologica Italica, la Società Gestione Ricerca Ambientale Ittica Acque,
-					l’Associazione Teriologica Italiana.</p>
+				<p>Dall’entrata in vigore della Direttiva nell’ambito del <a href="https://ec.europa.eu/environment/nature/knowledge/rep_habitats/index_en.htm" target="_blank">reporting ex art. 17</a> gli Stati membri hanno prodotto 
+					quattro Rapporti Nazionali, di cui gli ultimi tre comprensivi di valutazioni dello stato di 
+					conservazione di specie e habitat per il periodo di riferimento 
+					(2° Report: 2001-2006; 3° Report: 2007-2012; 4° Report: 2013-2018).  
+					Sulla base dei Rapporti nazionali la <b>Commissione Europea</b> e l’<b>Agenzia Europea per l’Ambiente</b> 
+					producono i <a href="https://circabc.europa.eu/faces/jsp/extension/wai/navigation/container.jsp" target="_blank">National Summary</a>,  le dashboard, le valutazioni a scala biogeografica e 
+					la sintesi dei risultati a livello europeo, pubblicata ogni sei anni nel Report <a href="https://www.eea.europa.eu/themes/biodiversity/state-of-nature-in-the-eu" target="_blank"><b>State of Nature in the EU</b></a></p>
+				
+				<p>Gli Attori coinvolti nelle attività di monitoraggio e reporting in Italia sono 
+					il Ministero della Transizione Ecologica, le Regioni e le Province Autonome di Trento e Bolzano e l’<b>ISPRA</b> che, 
+					dal 2011, coordina le attività per la realizzazione dei Rapporti Nazionali ex Art. 17 e 
+					fornisce Linee guida e strumenti tecnico-scientifici per la realizzazione dei monitoraggi. 
+					Per conseguire tali finalità l’ISPRA si avvale della collaborazione delle 
+					principali <b>Società Scientifiche nazionali</b>, quali: la <a href="https://www.societabotanicaitaliana.it/" target="_blank">Società Botanica Italiana</a>, 
+					l’<a href="http://www.uzionlus.it/" target="_blank">Unione Zoologica Italiana</a>, <a href="http://www.unipv.it/webshi/" target="_blank">la Societas Herpetologica Italica</a>, 
+					la <a href="http://www.graia.eu/" target="_blank">Società Gestione Ricerca Ambientale Ittica Acque</a>, l’<a href="https://www.mammiferi.org/" target="_blank">Associazione Teriologica Italiana</a></p>
 				
 				
             </div>  
