2975f8c6e1790d9c19872f22ddd7732f16404023
Ivan2nn <i.bernabucci@gmail.com>
2023-03-19 16:43:05 +0100

    first mods of requests for document of 30 January

--- src/resources/views/basic/downloads.blade.php
+++ src/resources/views/basic/downloads.blade.php
@@ -40,11 +40,11 @@
 		<p class="c-font-8"><a href="http://cdr.eionet.europa.eu/help/habitats_art17" target="_blank">http://cdr.eionet.europa.eu/help/habitats_art17</a></p>
                     <ul class="c-content-list-1 c-theme c-separator-dot">
                     <!-- <li><a href="http://www.sinanet.isprambiente.it/it/Reporting_Dir_Habitat/rapporto/rapporto-..." target="_blank">Volume del 3° Rapporto Nazionale Direttiva Habitat – Specie e habitat di interesse comunitario in Italia: distribuzione, stato di conservazione e trend. ISPRA, Serie Rapporti, 194/2014</a></li> -->
-			<li><a href="{!! asset('/documenti ufficiali europei/Reporting guidelines.pdf') !!}" target="_blank">Reporting guidelines (pdf)</a></li>
-			<li><a href="{!! asset('/documenti ufficiali europei/Report format.pdf') !!}" target="_blank">Reporting format (pdf)</a></li>
-			<li><a href="{!! asset('/documenti ufficiali europei/Art17_checklists_2019.xls') !!}" target="_blank">Checklist per specie ed habitat (xls)</a></li>
-			<li><a href="{!! asset('/documenti ufficiali europei/List of Pressures_Threats.xls') !!}" target="_blank">List of pressures and threats (xls)</a></li>
-			<li><a href="{!! asset('/documenti ufficiali europei/list_of_conservation_measures.xls') !!}" target="_blank">List of conservation measures (xls)</a></li>
+			<li><a href="{!! asset('/documents/documenti ufficiali europei/Reporting guidelines.pdf') !!}" target="_blank">Reporting guidelines (pdf)</a></li>
+			<li><a href="{!! asset('/documents/documenti ufficiali europei/Report format.pdf') !!}" target="_blank">Reporting format (pdf)</a></li>
+			<li><a href="{!! asset('/documents/documenti ufficiali europei/Art17_checklists_2019.xls') !!}" target="_blank">Checklist per specie ed habitat (xls)</a></li>
+			<li><a href="{!! asset('/documents/documenti ufficiali europei/List of Pressures_Threats.xls') !!}" target="_blank">List of pressures and threats (xls)</a></li>
+			<li><a href="{!! asset('/documents/documenti ufficiali europei/list_of_conservation_measures.xls') !!}" target="_blank">List of conservation measures (xls)</a></li>
 			
                     </ul>
                 <h2 class="c-font-uppercase c-font-bold c-font-40 c-margin-b-20 c-margin-t-30">MANUALI DI MONITORAGGIO - SPECIE E HABITAT TERRESTRI</h2>
@@ -57,12 +57,12 @@
                     </ul>
 		<p><strong>SCHEDE DI RILEVAMENTO DI CAMPO</strong></p>
                     <ul class="c-content-list-1 c-theme c-separator-dot">
-			<li><a href="{!! asset('/schede di rilevamento di campo/Scheda Briofite.docx') !!}" target="_blank">Scheda briofite (doc)</a></li>
-			<li><a href="{!! asset('/schede di rilevamento di campo/Scheda Licheni.docx') !!}" target="_blank">Scheda licheni (doc)</a></li>
-			<li><a href="{!! asset('/schede di rilevamento di campo/SCHEDE di CAMPO_sp_veg_ISTRUZIONI.pdf') !!}" target="_blank">Indicazione per la compilazione piante vascolari (pdf)</a></li>
-			<li><a href="{!! asset('/schede di rilevamento di campo/Scheda Piante Vascolari.docx') !!}" target="_blank">Scheda piante vascolari (doc)</a></li>
-			<li><a href="{!! asset('/schede di rilevamento di campo/Indicazioni per la compilazione della scheda habitat.pdf') !!}" target="_blank">Indicazione per la compilazione scheda habitat (pdf)</a></li> 
-			<li><a href="{!! asset('/schede di rilevamento di campo/Scheda HABITAT.xlsx') !!}" target="_blank">Scheda habitat (xlsx)</a></li>
+			<li><a href="{!! asset('/documents/schede di rilevamento di campo/Scheda Briofite.docx') !!}" target="_blank">Scheda briofite (doc)</a></li>
+			<li><a href="{!! asset('/documents/schede di rilevamento di campo/Scheda Licheni.docx') !!}" target="_blank">Scheda licheni (doc)</a></li>
+			<li><a href="{!! asset('/documents/schede di rilevamento di campo/SCHEDE di CAMPO_sp_veg_ISTRUZIONI.pdf') !!}" target="_blank">Indicazione per la compilazione piante vascolari (pdf)</a></li>
+			<li><a href="{!! asset('/documents/schede di rilevamento di campo/Scheda Piante Vascolari.docx') !!}" target="_blank">Scheda piante vascolari (doc)</a></li>
+			<li><a href="{!! asset('/documents/schede di rilevamento di campo/Indicazioni per la compilazione della scheda habitat.pdf') !!}" target="_blank">Indicazione per la compilazione scheda habitat (pdf)</a></li> 
+			<li><a href="{!! asset('/documents/schede di rilevamento di campo/Scheda HABITAT.xlsx') !!}" target="_blank">Scheda habitat (xlsx)</a></li>
                     </ul>
 		<p><strong>APPROFONDIMENTI (SPECIE)</strong></p>
 		<ul class="c-content-list-1 c-theme c-separator-dot">
@@ -79,7 +79,7 @@
 <h2 class="c-font-uppercase c-font-bold c-font-40 c-margin-b-20 c-margin-t-30">MANUALE DI MONITORAGGIO - SPECIE E HABITAT MARINI</h2>
 
 <ul class="c-content-list-1 c-theme c-separator-dot">
-			<li><a href="{!! asset('/manuali_monitoraggio/MLG MARE_190_19.pdf') !!}" target="_blank">Manuali per il monitoraggio di specie e habitat di interesse comunitario (Direttiva 92/43/CEE e
+			<li><a href="{!! asset('/documents/manuali_monitoraggio/MLG MARE_190_19.pdf') !!}" target="_blank">Manuali per il monitoraggio di specie e habitat di interesse comunitario (Direttiva 92/43/CEE e
 				Direttiva 09/147/CE) in Italia: ambiente marino. ISPRA, Serie Manuali e linee guida, 190/2019.</a></li>
 			
 				</ul>
@@ -90,8 +90,8 @@
 		</ul>
 		<p><strong>APPROFONDIMENTI (HABITAT)</strong></p>
 		<ul class="c-content-list-1 c-theme c-separator-dot">
-			<li><a href="{!! asset('/La rilevanza delle regioni e PA nella conservazione degli habitat.pdf') !!}" target="_blank">Studio: Indice di rilevanza di una Regione sulla conservazione di un habitat a livello biogeografico</a></li>
-			<li><a href="{!! asset('/dati_regionali _indice rilevanza.xlsx') !!}" target="_blank">Indice di rilevanza: dati regionali</a></li>
+			<li><a href="{!! asset('/documents/La rilevanza delle regioni e PA nella conservazione degli habitat.pdf') !!}" target="_blank">Studio: Indice di rilevanza di una Regione sulla conservazione di un habitat a livello biogeografico</a></li>
+			<li><a href="{!! asset('/documents/dati_regionali _indice rilevanza.xlsx') !!}" target="_blank">Indice di rilevanza: dati regionali</a></li>
 			
 		</ul>
             </div>  
